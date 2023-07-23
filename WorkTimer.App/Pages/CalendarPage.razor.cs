a5d04222e56ad5cf7c11a905e57bd8fa7f7c19dd
MJSasha <sasha.ushankov@gmail.com>
2023-07-23 17:27:00 +0300

    Add round indicator

--- WorkTimer.App/Pages/CalendarPage.razor.cs
+++ WorkTimer.App/Pages/CalendarPage.razor.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
+using WorkTimer.App.Services;
 
 namespace WorkTimer.App.Pages
 {
     [Authorize]
     public partial class CalendarPage : ComponentBase
     {
+        [Inject]
+        protected ExceptionsHandler exceptionsHandler { get; set; }
+
         private DateTime SelectedDate { get; set; }
         private bool IsLoading { get => isLoading; set { isLoading = value; StateHasChanged(); } }
 
@@ -16,6 +20,29 @@ namespace WorkTimer.App.Pages
             await base.OnInitializedAsync();
 
             SelectedDate = DateTime.Today;
+            await LoadData();
+        }
+
+        private async Task LoadData()
+        {
+            IsLoading = true;
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                await exceptionsHandler.Handle(ex);
+            }
+
+            IsLoading = false;
+        }
+
+        private async Task AddMonth(int monthCount)
+        {
+            SelectedDate = SelectedDate.AddMonths(monthCount);
+            await LoadData();
         }
     }
 }
